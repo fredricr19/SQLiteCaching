08b4127cdc872aa72f43660fd3c0314fa567afc9
fredricr19 <fredricr19@up.edu>
2018-02-15 16:36:23 -0800

    Update test_caching.py

--- test_caching.py
+++ test_caching.py
@@ -4,11 +4,19 @@ import getURL, caching
 def test_caching():
     url = "https://rtsfred3.github.io/echoo/"
     assert getURL.getURL(url) == caching.getHTML(url)
-    
+
 def test_caching2():
     url = "https://rtsfred3.github.io/echoo/index2.html"
     assert getURL.getURL(url) == caching.getHTML(url)
-    
+
 def test_caching3():
     url = "http://www.githubstat.us/"
+    assert getURL.getURL(url) == caching.getHTML(url)
+
+def test_caching4():
+    url = "https://library.up.edu/"
+    assert getURL.getURL(url) == caching.getHTML(url)
+
+def test_caching5():
+    url = "https://www.up.edu/"
     assert getURL.getURL(url) == caching.getHTML(url)
